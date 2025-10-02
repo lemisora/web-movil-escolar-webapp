f1d063f3b527bb818a1fcc4bfc72ecd14fbf383e
lemisora <leonardo.michel.dom@gmail.com>
2025-10-02 17:18:53 -0600

    Corrección de campos y método para reconocer letras

--- src/app/partials/registro-admin/registro-admin.component.ts
+++ src/app/partials/registro-admin/registro-admin.component.ts
@@ -1,90 +1,91 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FacadeService } from 'src/app/services/facade.service';
-import { Location } from '@angular/common';
-import { AdministradoresService } from 'src/app/services/administradores.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FacadeService } from "src/app/services/facade.service";
+import { Location } from "@angular/common";
+import { AdministradoresService } from "src/app/services/administradores.service";
 
 @Component({
-  selector: 'app-registro-admin',
-  templateUrl: './registro-admin.component.html',
-  styleUrls: ['./registro-admin.component.scss'],
-  standalone: false
+  selector: "app-registro-admin",
+  templateUrl: "./registro-admin.component.html",
+  styleUrls: ["./registro-admin.component.scss"],
+  standalone: false,
 })
 export class RegistroAdminComponent implements OnInit {
-
   @Input() rol: string = "";
   @Input() datos_user: any = {};
 
   public admin: any = {};
   public errors: any = {};
   public editar: boolean = false;
-  public token :string = "";
+  public token: string = "";
   public idUser: number = 0;
-  
+
   // Para las contraseñas
   public hide_1: boolean = false;
   public hide_2: boolean = false;
   public inputType_1: string = "password";
   public inputType_2: string = "password";
-  
+
   constructor(
     private location: Location,
     private administradoresService: AdministradoresService,
     public acivatedRoute: ActivatedRoute,
     private facadeService: FacadeService,
-    private router: Router
-  ) { }
+    private router: Router,
+  ) {}
 
-  ngOnInit(): void {
-  }
-  
+  ngOnInit(): void {}
+
   // Funciones para contraseña
-  public showPassword() { 
-    if (this.inputType_1 == 'password') {
-      this.inputType_1 = 'text';
+  public showPassword() {
+    if (this.inputType_1 == "password") {
+      this.inputType_1 = "text";
       this.hide_1 = true;
-    } else { 
-      this.inputType_1 = 'password';
+    } else {
+      this.inputType_1 = "password";
       this.hide_1 = false;
     }
   }
-  
-  public showPwdConfirmar() { 
-    if (this.inputType_2 == 'password') {
-      this.inputType_2 = 'text';
+
+  public showPwdConfirmar() {
+    if (this.inputType_2 == "password") {
+      this.inputType_2 = "text";
       this.hide_2 = true;
-    } else { 
-      this.inputType_2 = 'password';
+    } else {
+      this.inputType_2 = "password";
       this.hide_2 = false;
     }
   }
 
-  public regresar() { 
+  public regresar() {
     this.location.back();
   }
-  
-  public registrar() { 
+
+  public registrar() {
     this.errors = {};
     this.errors = this.administradoresService.validarAdmin(
-      this.admin, this.editar
+      this.admin,
+      this.editar,
     );
-    if(Object.keys(this.errors).length > 0) {
+    if (Object.keys(this.errors).length > 0) {
       return false;
     }
-    console.log("Pasó la validación")
+    console.log("Pasó la validación");
   }
-  
-  public actualizar() { }
-  
-  public soloLetras(event: KeyboardEvent) { 
+
+  public actualizar() {}
+
+  public soloLetras(event: KeyboardEvent) {
     const charCode = event.key.charCodeAt(0);
+
+    // Permitir solo letras (mayúsculas y minúsculas) y espacio
+
     if (
-      ! (charCode < 65 || charCode > 90) && // Letras mayúsculas
-      ! (charCode > 97 && charCode < 122 ) && // Letras minúsculas
-      ! (charCode !== 32) // Espacio
-    ) { 
+      !(charCode >= 65 && charCode <= 90) && // Letras mayúsculas
+      !(charCode >= 97 && charCode <= 122) && // Letras minúsculas
+      charCode !== 32 // Espacio
+    ) {
       event.preventDefault();
     }
   }
-  
 }
